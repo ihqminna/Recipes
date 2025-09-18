e70dcd83373581c51f79f5d4e7e7c050d7493dc7
ihqminna <minna.iholin@welho.com>
2025-09-19 00:12:20 +0300

    Created recipes.py for recipe functions

---
+++ recipes.py
@@ -0,0 +1,25 @@
+import db
+
+def recipe_name_free(name):
+    sql = "SELECT name FROM recipes WHERE name=?"
+    result = db.query(sql, [name])
+    if result:
+        return False
+    else:
+        return True
+
+def create_slug(name):
+    slug = ""
+    for i in name:
+        if i == "ä":
+            add = "a"
+        elif i == "ö":
+            add = "o"
+        elif i.isalpha():
+            add = i
+        elif i == " ":
+            add = "_"
+        else:
+            add = ""
+        slug = slug + add
+    return slug
